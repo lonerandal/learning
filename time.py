b9839ac1d7f94309e1a4f53018e1d907ebd74350
Randal <randal@RandaldeMacBook-Pro.local>
2018-02-23 09:19:13 +0800

    update

--- time.py
+++ time.py
@@ -12,12 +12,16 @@ def string_toTimestamp(strTime):
 
 #把时间戳转成字符串形式
 def timestamp_toString(stamp):
-    return time.strftime("%Y-%m-%d-%H", tiem.localtime(stamp))
+    return time.strftime("%Y-%m-%d-%H", time.localtime(stamp))
 
 #把datetime类型转外时间戳形式
 def datetime_toTimestamp(dateTim):
     return time.mktime(dateTim.timetuple())
+#j将时间戳（int）转成datatime.date格式
+def timestamp_datetime(stamp):
+    datetime.datetime.fromtimestamp(stamp).date()
 
+datetime.datetime.strptime('2018-02-10', "%Y-%m-%d").date()
 
 import datetime
 from dateutil.relativedelta import relativedelta
